90d749679a5092a8e06613ce805cc499b34ab51b
Vuducnham1123 <vuducnham1123@gmail.com>
2025-06-22 22:04:33 +0700

    commi1

---
+++ Test-ci-cd-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Test_ci_cd_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
